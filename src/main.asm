bc441c0eabf8d6d73403cf62a3b41122000824d4
debater-coder <52619668+debater-coder@users.noreply.github.com>
2023-10-02 07:27:34 +1100

    simple prompt

--- src/main.asm
+++ src/main.asm
@@ -7,6 +7,28 @@ start:
         jmp main
 
 ;
+; Get a character of input and echo it to the screen
+;
+; Returns:
+;     - al contains ASCII keycode
+input:
+        ; save registers we will modify
+        push bx
+
+        ; get input
+        mov ah, 0
+        int 0x16
+
+        ; echo to screen
+        mov bh, 0
+        mov ah, 0x0e
+        int 0x10
+
+        ; return
+        pop bx
+        ret
+
+;
 ; Prints a null-terminated string to the screen
 ; Params:
 ;     - ds:si points to string
@@ -44,16 +66,32 @@ main:
         mov ss, ax
         mov sp, 0x7C00
 
-        mov si, msg_hello_world
+        mov si, msg_boot_success
         call print
         
+        mov si, msg_prompt
+        call print
+        
+.loop:
+        ; get input
+        call input
+
+        ; check if enter
+        cmp al, 13
+        jne .loop
+
+        mov si, msg_prompt
+        call print        
+            
+        jmp .loop        
         
         hlt
 
 .halt:
         jmp .halt
 
-msg_hello_world: db 'Hello, World!', ENDL, 0
+msg_boot_success: db 'Bootloader v0.1.0 ran successfully.', ENDL, 0
+msg_prompt: db ENDL, '> ', 0
 
 times 510-($-$$) db 0
 dw 0AA55h
